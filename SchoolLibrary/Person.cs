e439aad9a339caff399521295dde7cf1627076d7
Louis Choo-Choy <lchoochoy@gmail.com>
2020-06-21 23:18:18 +0100

    first and only commit

---
+++ SchoolLibrary/Person.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace SchoolLibrary
+{
+    public abstract class Person
+     //Abstract means you can't instantiate this class directly
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ SchoolLibrary/Teacher.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace SchoolLibrary
+{
+    public class Teacher : Person
+     //Inherits from person base class
+    {
+        public string Subject { get; set; }
+    }
+}
